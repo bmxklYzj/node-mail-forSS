aad601f39d4ac3953eacb00fc9e4031a06df80dd
yangzongjun <yangzongjun@baidu.com>
2017-11-21 17:32:45 +0800

    fix filename

--- mail/index.js
+++ mail/index.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 
 // Generate test SMTP service account from ethereal.email
 const account = require('../config');
-let receiversConfig = require('./receiversConfigConfig');
+let receiversConfig = require('./receiversConfig');
 
 receiversConfig = receiversConfig.join(',');
 
