8b2e32cbdc0e5407930a1b01e17fd6ac58d3ad30
yangzongjun <yangzongjun@baidu.com>
2017-12-01 22:07:21 +0800

    modify read

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ fs.writeFile('/etc/shadowsocks.json', configString, (err) => {
       console.log(stdout);
     });
     sendMail({
-        html: configString + '<br> zongjun祝您 fanqiang 愉快。<br>如果你觉得此项目还不错，可以给我点个star。 =='
+        html: configString + '<br> zongjun祝您 fanqiang 愉快。<br>如果你觉得此项目还不错，可以给我点个star。链接： https://github.com/bmxklYzj/node-mail-forSS  =='
     });
 });
 console.log(configString);
